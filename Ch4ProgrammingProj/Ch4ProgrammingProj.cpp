eb605ddb76961df5309ecbb301282b70b1bde1b6
me <yuh@bruh.com>
2024-10-17 23:23:46 -0400

    PHEW wow. program tested. works.

--- Ch4ProgrammingProj/Ch4ProgrammingProj.cpp
+++ Ch4ProgrammingProj/Ch4ProgrammingProj.cpp
@@ -1,11 +1,58 @@
 // Ch4ProgrammingProj.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*shipping project:
+* 
+* RATES PER 500mi:
+<=2kg $1.10
+>2kg, <=6kg = $2.20
+>6kg <=10kg = $3.70
+>10kg <=20kg = $4.80
+need to calculate per 500mi, so need to 
+
+ask for weight+distance, calc total*/
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    
+    double weight, Dis, cost, rate;
+    cout << "Please input your package weight in kg and how far away you wish to send it in miles.\nMAX WEIGHT: 20kg\nMIN DISTANCE: 10mi\nMAX DISTANCE: 3000mi" << endl;
+        cout << "WEIGHT:" << endl;
+        cin >> weight;
+            cout << "DISTANCE:" << endl;
+            cin >> Dis;
+
+    if (weight > 0 && weight <= 2)
+    {
+        rate = 1.10;
+    }
+        else if (weight > 2 && weight <= 6)
+        {
+            rate = 2.20;
+        }
+            else if (weight > 6 && weight <= 10)
+            {
+                rate = 3.70;
+            }
+                else if (weight > 10 && weight <= 20)
+                {
+                    rate = 4.80;
+                }
+                    else
+                    {
+                        cout << "ERROR: Weight cannot be zero or exceed 20kg." << endl;
+                    }
+    if (Dis < 10 || Dis > 3000)
+    {
+        cout << "ERROR: Your distance has not met the minimum or maximum." << endl;
+    }
+        else
+        {
+            cost = rate * ((Dis + 499) / 500.0);
+            cout << "Your shipping charge is estimated to be: " << setprecision(3) <<  cost << endl;
+        }
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
