6368f5f309b9ae1ae66b112475852f9c969586a8
me <yuh@bruh.com>
2024-10-17 21:53:12 -0400

    prog ran andtested

--- Ch4ProgrammingProj/Ch4ProgrammingProj.cpp
+++ Ch4ProgrammingProj/Ch4ProgrammingProj.cpp
@@ -1,11 +1,61 @@
 // Ch4ProgrammingProj.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*define which months have 30 and 31 days, as well as feb having 28
+prompt user to enter month in integer 1-12
+calculate if leap yr (bool? something)
+cin month number, invalidate/return to menu if any other number than 1-12 are entered
+cin year
+if monthsw30days, display 30 days. if monthsw31days, display 31. 
+feb is its own at 28, 29 if leap yr (if year%100 or 4 == 0, display its a leap yr)
+*/
 
 #include <iostream>
+using namespace std;
+
+bool isLeap(int year)
+{
+    if (year % 100 == 0)
+    {
+        return year % 400 == 0;
+    }
+    return year % 4 == 0;
+}
 
 int main()
+
 {
-    
+    int year, month;
+
+    cout << "Please enter the month you want as any number 1-12." << endl;
+    cin >> month;
+    cout << "Please enter a year." << endl;
+    cin >> year;
+    if (month == 0 || month > 12)
+    {
+        cout << "Invalid month number.";
+        return 1;
+    }
+
+    int daisyn;
+    switch (month)
+    {case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        daisyn = 31;
+        break;
+    case 4: case 6: case 9: case 11:
+        daisyn = 30;
+        break;
+    case 2:
+        if (isLeap(year))
+        {
+            daisyn = 29;
+        }
+        else
+        {
+            daisyn = 28;
+        }
+        break;
+    }
+    cout << "Month: " << month << '\n' << "Number of days: " << daisyn << '\n' << "Year: " << year << endl;
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
